f93d43dec154f92514f56a2b8bc9e22829dcb9f7
dr.w <dr.glasdou@gmail.com>
2023-05-06 20:56:29 -0600

    change: BaseEntity -> CustomBaseEntity

--- src/lists/entities/list.entity.ts
+++ src/lists/entities/list.entity.ts
@@ -4,11 +4,11 @@ import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 import { Board } from 'src/boards/entities/board.entity';
 import { User } from 'src/users/entities/user.entity';
-import { BaseEntity } from './../../common/entities/base.entity';
+import { CustomBaseEntity } from '../../common/entities/custom-base.entity';
 
 @ObjectType()
 @Entity('lists')
-export class List extends BaseEntity {
+export class List extends CustomBaseEntity {
   @Field(() => String)
   @Column({ type: 'text' })
   @MaxLength(1000)
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,13 +2,13 @@ import { UserRoles } from './../../common/types/user-roles';
 import { Field, ObjectType } from '@nestjs/graphql';
 import { MaxLength, MinLength } from 'class-validator';
 import { Column, Entity, Index, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
-import { BaseEntity } from './../../common/entities/base.entity';
+import { CustomBaseEntity } from '../../common/entities/custom-base.entity';
 
 @ObjectType()
 @Entity('users')
 @Index('user_email_IX', ['email'], { unique: true })
 @Index('user_username_IX', ['username'], { unique: true })
-export class User extends BaseEntity {
+export class User extends CustomBaseEntity {
   @Field(() => String)
   @Column({ type: 'text' })
   @MaxLength(100)
